26894cdcebabeae54571f987c0942e6cec8320b2
khang <chaoskid1415@gmail.com>
2025-07-02 16:08:10 +0700

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,66 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {
-    HomeLayout,
-    About,
-    Cocktail,
-    Landing,
-    Newsletter,
-    Error,
-} from "./pages";
-import { loader as landingLoader } from "./pages/Landing.jsx";
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+  SinglePageError,
+} from './pages';
+
+import { loader as landingLoader } from './pages/Landing';
+import { loader as singleCocktailLoader } from './pages/Cocktail';
+import { action as newsletterAction } from './pages/Newsletter';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
-    {
-        path: `/`,
-        element: <HomeLayout />,
-        errorElement: <Error />,
-        children: [
-            {
-                index: true,
-                loader: landingLoader,
-                element: <Landing />,
-            },
-            {
-                path: `cocktail`,
-                element: <Cocktail />,
-            },
-            {
-                path: `newsletter`,
-                element: <Newsletter />,
-            },
-            {
-                path: `about`,
-                element: <About />,
-            },
-        ],
-    },
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <SinglePageError />,
+        loader: landingLoader(queryClient),
+      },
+      {
+        path: 'cocktail/:id',
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader(queryClient),
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+        action: newsletterAction,
+        errorElement: <SinglePageError />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
 ]);
 
 const App = () => {
-    return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 export default App;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,15 +1,22 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import { Outlet, useNavigation } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
-function HomeLayout() {
-    return (
-        <div>
-            <Navbar />
-            <section className="page">
-                <Outlet />
-            </section>
-        </div>
-    );
-}
+const HomeLayout = () => {
+  const navigation = useNavigation();
 
+  const isPageLoading = navigation.state === 'loading';
+  const value = 'some value';
+  return (
+    <>
+      <Navbar />
+      <section className='page'>
+        {isPageLoading ? (
+          <div className='loading' />
+        ) : (
+          <Outlet context={{ value }} />
+        )}
+      </section>
+    </>
+  );
+};
 export default HomeLayout;
